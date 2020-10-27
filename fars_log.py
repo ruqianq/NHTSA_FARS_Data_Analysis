1b92912051e4423400178d0f596a3b128538ca87
Daisy Qin <dqin@onyxgs.com>
2020-10-27 13:22:27 -0400

    add the sklearn library

--- fars_log.py
+++ fars_log.py
@@ -1,1 +1,13 @@
-print("dfdf")
+from sklearn.linear_model import SGDClassifier
+
+# Training data
+X = dm_traindf.loc[:, dm_input]
+
+# Labels
+y = dm_traindf[dm_dec_target]
+
+dm_model = SGDClassifier(loss="hinge", penalty="l2", max_iter=5)
+
+dm_model.fit(X, y)
+
+print(dm_model)
