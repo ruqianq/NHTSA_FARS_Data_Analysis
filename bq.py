0745d6fd200dbde391f6f69c018a610398001fb8
Daisy Qin <dqin@onyxgs.com>
2020-11-06 12:05:10 -0500

    clean data

--- bq.py
+++ bq.py
@@ -2,7 +2,8 @@ import pandas as pd
 from google.cloud import bigquery
 
 # Construct a BigQuery client object.
-# Follow the instruction: https://cloud.google.com/bigquery/docs/reference/libraries?authuser=1#client-libraries-usage-python
+# Follow the instruction:
+# https://cloud.google.com/bigquery/docs/reference/libraries?authuser=1#client-libraries-usage-python
 
 
 def to_df() -> pd.DataFrame:
--- fars_sk.py
+++ fars_sk.py
@@ -1,30 +1,19 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import bq
 
-df = pd.read_csv('data_sample.csv')
+df = bq.to_df()
 
 # Data cleanse
 
 df_cln = df[df.columns[df.isnull().mean() < 0.5]]
-
-df_cln["ped_death"] = df.apply(lambda row: 1 if (row.fatals - row.deaths > 0) else 0, axis=1)
+df_cln["ped_death"] = df_cln.apply(lambda row: 1 if (row.number_of_fatalities - row.fatalities_in_vehicle > 0) else 0,
+                                   axis=1)
 print(df_cln["ped_death"])
 
-X = df_cln.drop(["ped_death"], axis=1)
-y = df_cln["ped_death"]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-# Viya Open Source Code Node
-# # Training data
-# X = dm_traindf.loc[:, dm_input]
+# X = df_cln.drop(["ped_death"], axis=1)
+# y = df_cln["ped_death"]
 #
-# # Labels
-# y = dm_traindf[dm_dec_target]
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 #
-# Fit RandomForest model w/ training data
-dm_model = RandomForestClassifier(n_estimators=10)
-dm_model.fit(X_train, y_train)
-
-print(pd.DataFrame(dm_model.predict(X_test), columns=['P_Injury_0']))
