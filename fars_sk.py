ecfcc9b3d6c9b6781d67da726a68e656cc58a3d0
Daisy Qin <dqin@onyxgs.com>
2020-11-09 14:54:08 -0500

    install matplotlib train multiple classifiers plot ROC

--- fars_sk.py
+++ fars_sk.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import roc_curve, roc_auc_score
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
 
 # query = """
 #     SELECT
@@ -13,13 +18,13 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 #     related_factors_driver_level_3, related_factors_driver_level_4, roadway_alignment, roadway_grade,
 #     roadway_surface_type, roadway_surface_condition, crash_type, fatalities_in_vehicle
 #     FROM (
-#     SELECT a.state_number, a.consecutive_number,
-#     number_of_forms_submitted_for_persons_not_in_motor_vehicles, a.county, city, a.day_of_crash, a.month_of_crash,
-#     day_of_week, a.hour_of_crash, national_highway_system, a.land_use, a.functional_system,
-#     route_signing, a.first_harmful_event, a.manner_of_collision, relation_to_junction_within_interchange_area,
-#     relation_to_junction_specific_location, type_of_intersection, relation_to_trafficway, light_condition,
-#     atmospheric_conditions, related_factors_crash_level_1, related_factors_crash_level_2, related_factors_crash_level_3,
-#     number_of_fatalities, p.vehicle_number, p.person_number, p.rollover, p.age, p.sex, p.person_type, p.injury_severity,
+#     SELECT a.state_number, a.consecutive_number,number_of_forms_submitted_for_persons_not_in_motor_vehicles,
+#     a.county, city, a.day_of_crash, a.month_of_crash, day_of_week, a.hour_of_crash, national_highway_system,
+#     a.land_use, a.functional_system, route_signing, a.first_harmful_event, a.manner_of_collision,
+#     relation_to_junction_within_interchange_area, relation_to_junction_specific_location, type_of_intersection,
+#     relation_to_trafficway, light_condition, atmospheric_conditions, related_factors_crash_level_1,
+#     related_factors_crash_level_2, related_factors_crash_level_3, number_of_fatalities, p.vehicle_number,
+#     p.person_number, p.rollover, p.age, p.sex, p.person_type, p.injury_severity,
 #     p.police_reported_alcohol_involvement, p.police_reported_drug_involvement, related_factors_person_level1,
 #     related_factors_person_level2, related_factors_person_level3, race
 #     FROM
@@ -97,22 +102,45 @@ y = df_cln["ped_death"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
-# Logistic Regression
+classifiers = [LogisticRegression(random_state=1234),
+               GaussianNB(),
+               KNeighborsClassifier(),
+               DecisionTreeClassifier(random_state=1234),
+               RandomForestClassifier(random_state=1234)]
 
-log_classifier = LogisticRegression(random_state=0)
-log_classifier.fit(X_train, y_train)
-y_pred = log_classifier.predict(X_test)
+result_table = pd.DataFrame(columns=['classifiers', 'fpr', 'tpr', 'auc'])
 
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-print(accuracy_score(y_test, y_pred))
+for cls in classifiers:
+    model = cls.fit(X_train, y_train)
+    yproba = model.predict_proba(X_test)[::, 1]
 
-# Decision Tree
+    fpr, tpr, _ = roc_curve(y_test, yproba)
+    auc = roc_auc_score(y_test, yproba)
 
-dt_classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
-dt_classifier.fit(X_train, y_train)
-y_pred = dt_classifier.predict(X_test)
+    result_table = result_table.append({'classifiers': cls.__class__.__name__,
+                                        'fpr': fpr,
+                                        'tpr': tpr,
+                                        'auc': auc}, ignore_index=True)
 
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-print(accuracy_score(y_test, y_pred))
+# Set name of the classifiers as index labels
+result_table.set_index('classifiers', inplace=True)
+
+fig = plt.figure(figsize=(8,6))
+
+for i in result_table.index:
+    plt.plot(result_table.loc[i]['fpr'],
+             result_table.loc[i]['tpr'],
+             label="{}, AUC={:.3f}".format(i, result_table.loc[i]['auc']))
+
+plt.plot([0,1], [0,1], color='orange', linestyle='--')
+
+plt.xticks(np.arange(0.0, 1.1, step=0.1))
+plt.xlabel("False Positive Rate", fontsize=15)
+
+plt.yticks(np.arange(0.0, 1.1, step=0.1))
+plt.ylabel("True Positive Rate", fontsize=15)
+
+plt.title('ROC Curve Analysis', fontweight='bold', fontsize=15)
+plt.legend(prop={'size': 13}, loc='lower right')
+
+plt.show()
