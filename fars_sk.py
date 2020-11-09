d4e71ea9fda67f603f5de038d2dbf8b63ca99b21
Daisy Qin <dqin@onyxgs.com>
2020-11-09 12:18:52 -0500

    clean query

--- fars_sk.py
+++ fars_sk.py
@@ -5,17 +5,17 @@ import pandas as pd
 #     ap.*, body_type,
 #     vehicle_trailing, gross_vehicle_weight_rating, travel_speed,
 #     previous_recorded_crashes, previous_recorded_suspensions_and_revocations, previous_dwi_convictions,
-#     previous_speeding_convictions, speeding_related, related_factors_driver_level_1, related_factors_driver_level_2,
+#     previous_speeding_convictions, related_factors_driver_level_1, related_factors_driver_level_2,
 #     related_factors_driver_level_3, related_factors_driver_level_4, roadway_alignment, roadway_grade,
-#     roadway_surface_condition, crash_type, fatalities_in_vehicle
+#     roadway_surface_type, roadway_surface_condition, crash_type, fatalities_in_vehicle
 #     FROM (
 #     SELECT a.state_number, a.consecutive_number,
 #     number_of_forms_submitted_for_persons_not_in_motor_vehicles, a.county, city, a.day_of_crash, a.month_of_crash,
-#     day_of_week, a.hour_of_crash, national_highway_system, a.land_use, a.functional_system, ownership
-#     route_signing, a.first_harmful_event, a.manner_of_collision,
-#     relation_to_junction_specific_location, relation_to_trafficway, light_condition,
+#     day_of_week, a.hour_of_crash, national_highway_system, a.land_use, a.functional_system,
+#     route_signing, a.first_harmful_event, a.manner_of_collision, relation_to_junction_within_interchange_area,
+#     relation_to_junction_specific_location, type_of_intersection, relation_to_trafficway, light_condition,
 #     atmospheric_conditions, related_factors_crash_level_1, related_factors_crash_level_2, related_factors_crash_level_3,
-#     number_of_fatalities, p.vehicle_number, p.person_number, p.age, p.sex, p.person_type, p.injury_severity,
+#     number_of_fatalities, p.vehicle_number, p.person_number, p.rollover, p.age, p.sex, p.person_type, p.injury_severity,
 #     p.police_reported_alcohol_involvement, p.police_reported_drug_involvement, related_factors_person_level1,
 #     related_factors_person_level2, related_factors_person_level3, race
 #     FROM
@@ -28,7 +28,7 @@ import pandas as pd
 #     JOIN
 #     `bigquery-public-data.nhtsa_traffic_fatalities.vehicle_2015` v
 #     ON ap.vehicle_number = v.vehicle_number
-#     LIMIT 100
+#     WHERE speeding_related LIKE '%Yes%'
 #     """
 
 query = """
@@ -79,11 +79,14 @@ df_cln['police_reported_alcohol_involvement'] = df_cln['police_reported_alcohol_
 df_cln['police_reported_alcohol_involvement'] = df_cln.apply(
     lambda row: 1 if (row.police_reported_alcohol_involvement == 'Yes') else 0, axis=1)
 
+df_cln['police_reported_drug_involvement'] = df_cln['police_reported_drug_involvement'].str[0:3]
+df_cln['police_reported_drug_involvement'] = df_cln.apply(
+    lambda row: 1 if (row.police_reported_drug_involvement == 'Yes') else 0, axis=1)
+
 df_cln['sex'] = df_cln.apply(
     lambda row: 0 if (row.sex == 'Female') else 1, axis=1)
 
 print(df_cln.dtypes)
-print(df_cln['police_reported_alcohol_involvement'])
 
 # X = df_cln.drop(["ped_death"], axis=1)
 # y = df_cln["ped_death"]
